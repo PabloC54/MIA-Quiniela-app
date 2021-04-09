f5a86bdd77694cc04af3e561049a5852a5501aac
Pablo Cabrera <3091865790609@ingenieria.usac.edu.gt>
2021-04-09 14:47:06 -0500

    Inicio del proyecto

---
+++ quiniela-app/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="row">
+			<div className="col-md-4">columna</div>
+			<div className="col-md-4">columna</div>
+			<div className="col-md-4">columna</div>
+		</div>
+	);
+}
+
+export default App;
